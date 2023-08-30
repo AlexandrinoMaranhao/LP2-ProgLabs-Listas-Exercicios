da0e63e3be7196d47932ce64ee916fcc0a58b5f1
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-08-30 17:28:02 -0300

    Correção Cosmética em Laboratório 6 - Atividades1&2.java

--- Laboratório 6 - Atividades1&2.java
+++ Laboratório 6 - Atividades1&2.java
@@ -154,14 +154,18 @@ public class ContaTeste {
 	 PessoaFisica pf1 = new PessoaFisica("Joaquim", "03944565453-9");
 	 Cartao ct1 = new Cartao(134567, pf1);
 	 Conta c1 = new Conta(1, ct1, 3443, 500);
-	 
-	 c1.creditaSaldo(550);
-	 c1.debitaSaldo(50);
-	 c1.verificaSaldo(3443);
-	 c1.alteraCartao(134568, pf1);
-		
-	 System.out.println(c1.getCartao());
-	 System.out.println(c1);
+	 System.out.println("Status da Conta");
+		System.out.println(c1);
+	
+		c1.creditaSaldo(550);
+		c1.debitaSaldo(50);
+		System.out.println("\n");
+		c1.verificaSaldo(3443);
+		c1.alteraCartao(134568, pf1);
+
+		//System.out.println(c1.getCartao());
+		System.out.println("\nStatus da Conta");
+		System.out.println(c1);
 	}
 
 }
