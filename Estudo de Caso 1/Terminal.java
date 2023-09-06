72aa12fe93d7b92bc6b5320b8793434091e500c9
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-06 17:21:45 -0300

    mudanças cosméticas e de indentação em Terminal.java

--- Estudo de Caso 1/Terminal.java
+++ Estudo de Caso 1/Terminal.java
@@ -6,95 +6,85 @@ public class Terminal {
 	private Caixa meuCaixa;
 	private int modoAtual;
 	
-	public Terminal (CadastroContas bd)
-	{
+	public class Terminal {
+
+	private Caixa meuCaixa;
+	private int modoAtual;
+
+	public Terminal(CadastroContas bd) {
 		this.meuCaixa = new Caixa(this, bd);
 	}
-	
-	public void iniciaOperacao()
-	{
+
+	public void iniciaOperacao() {
 		int opcao;
-		
-		opcao =  this.getOpcao();
-		
-		while (opcao != 4)
-		{
-			switch(opcao) 
-			{
-				case 1: double saldo = this.meuCaixa.consultaSaldo(getInt("NÚMERO DA CONTA"), getInt("SENHA"));
-					if(saldo != -1)
-					{
+
+		opcao = this.getOpcao();
+
+		while (opcao != 4) {
+			switch (opcao) {
+			case 1:
+				double saldo = this.meuCaixa.consultaSaldo(getInt("NÚMERO DA CONTA"), getInt("SENHA"));
+				if (saldo != -1) {
 					System.out.println("SALDO ATUAL: CR$ " + saldo);
-					}
-					else
-					{
+				} else {
 					System.out.println("SENHA OU CONTA INVÁLIDA, IMPOSSÍVEL ACESSAR SALDO");
-					}
-				case 2:boolean b = this.meuCaixa.efetuaSaque(getInt("NÚMERO DA CONTA"), (double) getInt("VALOR"), getInt("SENHA"));
-					if(b)
-					{
+				}
+			case 2:
+				boolean b = this.meuCaixa.efetuaSaque(getInt("NÚMERO DA CONTA"), (double) getInt("VALOR"),
+						getInt("SENHA"));
+				if (b) {
 					System.out.println("RETIRE AS CÉDULAS DE DINHEIRO");
-					}
-					else
-					{
+				} else {
 					System.out.println("PEDIDO DE SAQUE RECUSADO, TENTE NOVAMENTE MAIS TARDE");
-					}
-					break;
-				case 3: 
-					this.meuCaixa.recarregarCaixa();
-					break;
+				}
+				break;
+			case 3:
+				this.meuCaixa.recarregarCaixa();
+				break;
 			}
 			opcao = getOpcao();
 		}
 	}
-	
-	public void setModo(int modo)
-	{
-	 if(modo == 0 || modo == 1)
-	 {
-		 this.modoAtual = modo;
-	 }
+
+	public void setModo(int modo) {
+		if (modo == 0 || modo == 1) {
+			this.modoAtual = modo;
+		}
 	}
-	
-	private int getOpcao()
-	{
+
+	private int getOpcao() {
 		int opcao;
-		do
-		{
-		 if(this.modoAtual == 1)
-		 {
-		   opcao = getInt("OPCÕES 1) CONSULTA SALDO, 2) SAQUE BANCÁRIO, 4) SAIR");
-			if(opcao != 1 || opcao != 2 || opcao != 4)
-			{
-				opcao = 0;
+		do {
+			if (this.modoAtual == 1) {
+				opcao = getInt("++ MENU DO CLIENTE ++ \nOPCÕES \n1) CONSULTA SALDO \n2) SAQUE BANCÁRIO \n4) SAIR");
+				if (opcao != 1 || opcao != 2 || opcao != 4) {
+					opcao = 0;
+				}
+			} else {
+				opcao = getInt("MODO SUPERVISOR \nOPÇÕES \n3) RECARREGAR FUNDOS DO CAIXA \n4) SAIR");
+				if (opcao != 3 || opcao != 4) {
+					opcao = 0;
+				}
 			}
-			}
-		 else
-		 {	
-		   opcao =  getInt("MODO SUPERVISOR OPÇÕES 3) RECARREGAR FUNDOS DO CAIXA, 4) SAIR");
-			 if(opcao != 3 || opcao != 4)
-			 {
-				opcao = 0; 
-			 }
-		 }
 		} while (opcao == 0);
 		return opcao;
 	}
-	
-	private int getInt(String string)
-	{
+
+	private int getInt(String string) {
 		Scanner r = new Scanner(System.in);
 		System.out.println("Entre com " + string);
-		
-		if(r.hasNextInt())
-		{
-			return r.nextInt();
-		}
-		else
-		{
-			String st = r.next();
-			System.out.println("ERRO NA LEITURA DE DADOS");
-			return 0;
+
+		try {
+			if (r.hasNextInt()) {
+				return r.nextInt();
+			} else {
+				String st = r.next();
+				System.out.println("ERRO NA LEITURA DE DADOS");
+				return 0;
+			}
+		} finally {
+			r.close();
+
 		}
 	}
 }
