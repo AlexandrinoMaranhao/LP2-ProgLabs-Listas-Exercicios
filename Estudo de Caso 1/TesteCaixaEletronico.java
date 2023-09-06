9b4f73bf3c876b4195c3e76b972e89192a1e5bde
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-06 18:18:49 -0300

    Update TesteCaixaEletronico.java

--- Estudo de Caso 1/TesteCaixaEletronico.java
+++ Estudo de Caso 1/TesteCaixaEletronico.java
@@ -20,3 +20,35 @@ public class TesteCaixaEletronico {
 	}
 	}
 }
+
+ Cliente clientes[] = new Cliente[6]; 
+	 //Conta;
+	 CadastroContas cad1 = new CadastroContas(5);
+	 //Terminal meuTerminal =  new Terminal(Conta bd.contas);
+	/*Cliente cl1 = new Cliente("João", "123456-7");
+		Cliente cl2 = new Cliente("Julio", "789101-1");
+		Cliente cl3 = new Cliente("Karina", "121314-1");
+		Cliente cl4 = new Cliente("Luis", "516171-8");
+		Cliente cl5 = new Cliente("Maria", "192021-2");
+		
+		Conta c1 = new Conta(1, cl1, 123, 750);
+		Conta c2 = new Conta(1, cl2, 456, 800);
+		Conta c3 = new Conta(1, cl3, 789, 850);
+		Conta c4 = new Conta(1, cl4, 101, 900);
+		Conta c5 = new Conta(1, cl5, 213, 950);*/
+	 
+	for(int i = 0; i < clientes.length; i++)
+	{
+		
+	}
+	 //bd.meuTerminal();	
+	}
+	
+	 /*System.out.println(c1);
+	 System.out.println(c2);
+	 System.out.println(c3);
+	 System.out.println(c4);
+	 System.out.println(c5);*/
+}
+
+
