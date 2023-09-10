1880609c3ce62c68121b2dad8b5508cbebd1469a
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-10 01:40:47 -0300

    Inicialização primária dos arrays de objetos em TesteCaixaEletronico.java

--- Estudo de Caso 1/TesteCaixaEletronico.java
+++ Estudo de Caso 1/TesteCaixaEletronico.java
@@ -7,8 +7,40 @@ import java.util.Scanner;
 
 public class TesteCaixaEletronico {
 	public static void main(String[] args) {
+
+
+		Cliente cl1, cl2, cl3, cl4, cl5;
+		Conta c1, c2, c3, c4, c5;
 		
-	Cliente clientes[] = new Cliente[6]; 
+		Cliente[] clientes = {cl1 = new Cliente("João", "123456-7"),cl2 = new Cliente("Julio", "789101-1"), cl3 = new Cliente("Karina", "121314-1"), cl4 = new Cliente("Luis", "516171-8"), cl5 = new Cliente("Maria", "192021-2")};
+		Conta[] contas = {c1 = new Conta(101, cl1, 123, 5500), c2 = new Conta(102, cl2, 123, 5500), c3 = new Conta(103, cl3, 123, 500), c4 = new Conta(104, cl4, 123, 500), c5 = new Conta(105, cl5, 123, 500)};
+		CadastroContas cad1 = new CadastroContas(5);
+		
+		for(int i = 0; i < contas.length; i++)
+		{
+			cad1.adicionaConta(c1);
+			cad1.adicionaConta(c2);
+			cad1.adicionaConta(c3);
+			cad1.adicionaConta(c4);
+			cad1.adicionaConta(c5);
+			
+		}
+		
+		Terminal term1 = new Terminal(cad1);
+		
+		term1.setModo(1);
+		term1.iniciaOperacao();
+
+
+
+
+
+
+
+
+
+		
+	/*Cliente clientes[] = new Cliente[6]; 
 	 Conta contas[] = new Conta[6];
 	 
 	 
@@ -35,7 +67,7 @@ public class TesteCaixaEletronico {
 		Conta c2 = new Conta(1, cl2, 456, 800);
 		Conta c3 = new Conta(1, cl3, 789, 850);
 		Conta c4 = new Conta(1, cl4, 101, 900);
-		Conta c5 = new Conta(1, cl5, 213, 950);*/
+		Conta c5 = new Conta(1, cl5, 213, 950);
 	 
 	for(int i = 0; i < clientes.length; i++)
 	{
