851561c0338f27273339a8428c0406bb53144bd1
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-22 18:18:02 -0300

    Create Item.java

---
+++ Exercício DOME /Item.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Constructor;
+
+public class Item {
+	private String titulo;
+	private int ano;
+	private double tempReproducao;
+	private boolean gotIt;
+	private String comentario;
+	
+	public Item(String titulo, int ano, double tempReproducao, boolean gotIt, String comentario) {
+		this.titulo =  titulo;
+		this.ano = ano;
+		this.tempReproducao = tempReproducao;
+		this.gotIt = gotIt;
+		this.comentario = comentario;
+	}
+	
+	
+	public void imprime(){
+		
+	}
+}
