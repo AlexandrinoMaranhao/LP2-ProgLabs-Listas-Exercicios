2035db877e846d496dc06027ae786a866e852d02
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-22 17:39:30 -0300

    Finalização de Ponto2D.java

--- Laboratório 7/Atividade_1/Ponto2D.java
+++ Laboratório 7/Atividade_1/Ponto2D.java
@@ -1,5 +1,45 @@
 package atividade1
 import java.lang.reflect.Constructor;
 
-public class Ponto2D{
+public class Ponto2D {
+  protected double eixoX;
+  protected double eixoY;
+
+	public Ponto2D(double eixoX, double eixoY) {
+		this.eixoX = eixoX;
+		this.eixoY = eixoY;
+	}
+
+	public Ponto2D() {
+		this(0.0, 0.0);
+	}
+
+	public Ponto2D(Ponto2D p) {
+		this(p.getEixoX(), p.getEixoY());
+	}
+
+	public double getEixoX() {
+		return this.eixoX;
+	}
+
+	public double getEixoY() {
+		return this.eixoY;
+	}
+
+	public void desloca(double dx, double dy) {
+		if (dx != 0.0) {
+			this.eixoX += dx;
+		}
+		if (dx != 0.0) {
+			this.eixoY += dy;
+		}
+	}
+
+	public Ponto2D somaPonto(double dx, double dy) {
+		return new Ponto2D(this.eixoX + dx, this.eixoY + dy);
+	}
+
+	public Ponto2D somaPonto(Ponto2D p) {
+		return new Ponto2D(this.eixoX + p.getEixoX(), this.eixoY + p.getEixoY());
+	}
 }
