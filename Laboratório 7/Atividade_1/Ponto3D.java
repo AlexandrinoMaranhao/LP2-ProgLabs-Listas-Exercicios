de1f5cf8ec9e0c43a9064cb7935963f27af3931d
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-22 17:41:46 -0300

    Finalização de Ponto3D.java

--- Laboratório 7/Atividade_1/Ponto3D.java
+++ Laboratório 7/Atividade_1/Ponto3D.java
@@ -1,1 +1,34 @@
+package atividade1;
 
+public class Ponto3D extends Ponto2D {
+		private double eixoZ;
+		
+		public Ponto3D(double eixoX, double eixoY, double eixoZ) {
+			super(eixoX, eixoY);
+			this.eixoZ = eixoZ;
+		}
+		
+		public Ponto3D() {
+			this(0.0, 0.0, 0.0);
+		}
+		
+		public double getEixoZ() {
+			return this.eixoZ;
+		}
+		
+		public Ponto3D(Ponto3D p){
+			this(p.getEixoX(), p.getEixoY(), p.getEixoZ());
+		}
+		
+		public void desloca(double dx, double dy, double dz){
+			super.desloca(dx, dy);
+			if(dz != 0.0) {
+				this.eixoZ += dz;
+			}
+		}
+		
+		public Ponto3D somaPonto(double dx, double dy, double dz) {
+		    return new Ponto3D(super.getEixoX() + dx, super.getEixoY() + dy, this.eixoZ + dz);
+		}
+		
+}
