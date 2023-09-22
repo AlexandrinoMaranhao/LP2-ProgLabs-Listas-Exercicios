d65c4d43cd0778b296b1910a004c06e9b8efc379
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-22 17:57:20 -0300

    Finalização(/?) em Pixel.java

--- Laboratório 7/Atividade_1/Pixel.java
+++ Laboratório 7/Atividade_1/Pixel.java
@@ -1,2 +1,28 @@
 package atividade1;
 
+public class Pixel extends Ponto2D{
+	private int valorCor;
+	
+	public Pixel(double eixoX, double eixoY, int valrCor) {
+		
+		super(eixoX, eixoY);
+		this.valorCor = valorCor%100;
+	}
+	
+	public Pixel() {
+		super();
+		this.valorCor = 0;
+	}
+	
+	public int getCor(){
+		return this.valorCor;
+	}
+	
+	public void mudaCor(int cor) {
+		this.valorCor =  cor;
+	}
+	
+	public void deslocaPixel(double dx, double dy) {
+		super.desloca(dx, dy);
+	}
+}
