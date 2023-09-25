2a4d7e7b57032e9acad084fe6d9dde7c747f3216
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-25 00:43:29 -0300

    Finalização do BancoDeDados.java

--- Exercício DOME /BancoDeDados.java
+++ Exercício DOME /BancoDeDados.java
@@ -1,5 +1,22 @@
+package dome
+
 import java.util.ArrayList;
 
 public class BancoDeDados {
+  private ArrayList<Item> itens;
 
+  public BancoDeDados(){
+    this.itens = new ArrayList<>();
+  }
+  
+ public void adicionaItem (Item item){
+    this.itens.add(item);
+  }
+
+ public void listaItens(){
+    for(Item item : this.itens){
+      item.imprime();
+      System.out.println();
+    }
+ }
 }
