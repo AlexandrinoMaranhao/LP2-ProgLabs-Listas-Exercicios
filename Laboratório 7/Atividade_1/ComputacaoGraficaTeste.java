2c55a1f40d3c84497e34888ff67c6d0cfb1ea881
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-25 17:14:24 -0300

    Finalização de parte inicial dos objetos teste em ComputacaoGraficaTeste.java

--- Laboratório 7/Atividade_1/ComputacaoGraficaTeste.java
+++ Laboratório 7/Atividade_1/ComputacaoGraficaTeste.java
@@ -4,7 +4,18 @@ public class ComputacaoGraficaTeste {
 
 	public static void main(String[] args) {
 		
-		Ponto2D pt1 = new Ponto2D();
+		Ponto2D pt1 = new Ponto2D(5.0, 5.0);
+		Ponto2D pt2 = new Ponto2D(7.0, 7.0);
+		Ponto2D pt3 = new Ponto2D();
+		
+		pt1.clone();
+		System.out.println(pt1);
+		System.out.println(pt1.clone());
+		System.out.println(pt2);
+		System.out.println(pt3);
+		
+		pt2.equals(pt1.clone());
+		System.out.println(pt2.equals(pt1.clone()));
 	}
 
 }
