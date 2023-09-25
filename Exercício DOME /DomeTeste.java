cfd438bb63977f9d31eab90ce956e367da011053
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-25 00:57:08 -0300

    Create DomeTeste.java

---
+++ Exercício DOME /DomeTeste.java
@@ -0,0 +1,12 @@
+package dome.ambiente
+
+public class DomeTeste{
+  public static void main(String[] args){
+    BancoDeDados meuBd = new BancoDeDados();
+    
+    meuBd.adicionaItem(new Album("As Quatro Estações", 1989, 120, true, "Marcou uma geração", "Legião Urbana", 10);
+    meuBd.adicionaItem(new Filme("Taxi Driver", 1976, 114, false, "Um dos grandes filmes estrelando Robert DeNiro", "Martin Scorsese");
+
+    meuBd.listaItens();
+  }
+}
