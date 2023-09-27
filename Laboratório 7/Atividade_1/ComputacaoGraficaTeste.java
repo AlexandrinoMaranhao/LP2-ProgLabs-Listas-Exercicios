aca35a82203b9acbbc9efd8d78f8042cbc73a749
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-27 16:29:01 -0300

    Mais sujeitos de teste em ComputacaoGraficaTeste.java

--- Laboratório 7/Atividade_1/ComputacaoGraficaTeste.java
+++ Laboratório 7/Atividade_1/ComputacaoGraficaTeste.java
@@ -7,12 +7,21 @@ public class ComputacaoGraficaTeste {
 		Ponto2D pt1 = new Ponto2D(5.0, 5.0);
 		Ponto2D pt2 = new Ponto2D(7.0, 7.0);
 		Ponto2D pt3 = new Ponto2D();
+		Ponto3D pt4 = new Ponto3D (1.0, 1.0, 1.0);
+		Pixel px = new Pixel(4.0, 4.0, 55);
+		 
+		pt1.desloca(4.0, 5.0);
+		pt2.desloca(5.0, 5.0, 5.0);
+		px.deslocaPixel(5.0, 5.0);
+		px.mudaCor(65);
+		System.out.println(px);
 		
 		pt1.clone();
 		System.out.println(pt1);
 		System.out.println(pt1.clone());
 		System.out.println(pt2);
 		System.out.println(pt3);
+		System.out.println(pt4);
 		
 		pt2.equals(pt1.clone());
 		System.out.println(pt2.equals(pt1.clone()));
