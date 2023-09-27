2d6c69bd4f161e52f806b047523c3cbdde5f8507
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-27 17:19:45 -0300

    Preenchimento da classe Lance.java

--- Estudo de Caso II /Lance.java
+++ Estudo de Caso II /Lance.java
@@ -1,5 +1,19 @@
 package poo.sistemadeleiloes;
 
 public class Lance {
+    private double valorLance;
+	  private Pessoa licitante;
+	
+	  public Lance(double lance, Pessoa licitante) {
+		this.valorLance = lance;
+		this.licitante = licitante;
+	  }
 
+	  public double getValorLance() {
+		return valorLance;
+  	}
+
+	  public Pessoa getLicitante() {
+		return licitante;
+  	}
 }
