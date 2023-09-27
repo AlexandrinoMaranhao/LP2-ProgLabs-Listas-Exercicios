811a7e7bb00ffcbed00c2f131c2aa5db66fde3b0
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-27 16:56:55 -0300

    Preenchimento da classe Pessoa.java

--- Estudo de Caso II /Pessoa.java
+++ Estudo de Caso II /Pessoa.java
@@ -1,5 +1,13 @@
 package poo.sistemadeleiloes;
 
 public class Pessoa {
-
+    private String nome;
+	
+	  public Pessoa(String licitante) {
+		this.nome = licitante;
+	  }
+	
+	  public String getNomeLicitante() {
+		return this.nome;
+	  }
 }
