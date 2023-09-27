4250aee61cc84e1e2a8f903c771a45fe53c42ea3
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-27 17:30:04 -0300

    Preenchimento parcial da classe Lote.java

--- Estudo de Caso II /Lote.java
+++ Estudo de Caso II /Lote.java
@@ -1,5 +1,28 @@
 package poo.sistemadeleiloes;
 
 public class Lote {
+  private int numeroLote;
+	private String descricao;
+	private Lance maiorLance;
 
+	public Lote(int numLote, String desc) {
+		this.numeroLote = numLote;
+		this.descricao = desc;
+	}
+
+	public int getNumeroLote() {
+		return this.numeroLote;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public Lance getMaiorLance() {
+		return this.maiorLance;
+	}
+
+	public void lancePara(double valor, Pessoa licitante) {
+
+	}
 }
