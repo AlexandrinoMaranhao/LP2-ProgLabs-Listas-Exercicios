3b1d63601a1b27e68df5652b4f02859f82e8f54e
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-09-27 17:28:48 -0300

    Correções em Lance.java

--- Estudo de Caso II /Lance.java
+++ Estudo de Caso II /Lance.java
@@ -1,19 +1,19 @@
 package poo.sistemadeleiloes;
 
 public class Lance {
-    private double valorLance;
+   	  private double valorLance;
 	  private Pessoa licitante;
 	
-	  public Lance(double lance, Pessoa licitante) {
-		this.valorLance = lance;
+	  public Lance(double valor, Pessoa licitante) {
+		this.valorLance = valor;
 		this.licitante = licitante;
 	  }
 
 	  public double getValorLance() {
-		return valorLance;
+		return this.valorLance;
   	}
 
 	  public Pessoa getLicitante() {
-		return licitante;
+		return this.licitante;
   	}
 }
