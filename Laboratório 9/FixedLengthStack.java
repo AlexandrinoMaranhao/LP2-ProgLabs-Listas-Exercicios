afb656385a9efac347242b9c81e7478bb68f1b86
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-10-03 15:49:12 -0300

    Finalização das novas implementações em FixedLengthStack.java

--- Laboratório 9/FixedLengthStack.java
+++ Laboratório 9/FixedLengthStack.java
@@ -8,27 +8,27 @@ public class FixedLengthStack implements ASimpleStack {
 		this.dadosDaPilha = new char[tamanho];
 		this.topo = 0;
 	}
-	
+
 	public FixedLengthStack() {
-		
+
 	}
 
 	public void push(char dado) {
 		if (isFull()) {
 			System.out.println("PILHA CHEIA");
 			return;
-		} 
+		}
 		this.dadosDaPilha[topo++] = dado;
-		
+
 	}
 
 	public char pop() {
 		if (isEmpty()) {
 			System.out.println("PILHA VAZIA");
 			return (char) 0;
-		} 
+		}
 		return this.dadosDaPilha[--topo];
-		
+
 	}
 
 	public boolean isEmpty() {
@@ -39,8 +39,22 @@ public class FixedLengthStack implements ASimpleStack {
 		return this.topo == this.dadosDaPilha.length;
 	}
 
-	
-	// void reset(){   };
-	// char peek(){  };
-	// int size(){ return this.dadosDaPilha.length };
+	public void reset() {
+		this.topo = 0;
+	}
+
+	public char peek() {
+		if (isEmpty()) {
+			System.out.println("PILHA VAZIA");
+			return (char) 0;
+		}
+		return this.dadosDaPilha[topo++];
+	}
+
+	public int size() {
+		if (isEmpty()) {
+			return (char) 0;
+		}
+		return this.dadosDaPilha.length;
+	}
 }
