7a1ccdec50e13efa9d42da3228fc61cb037ffc22
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-10-03 15:44:52 -0300

    Mais testes em ASimpleStackDemo.java

--- Laboratório 9/ASimpleStackDemo.java
+++ Laboratório 9/ASimpleStackDemo.java
@@ -2,10 +2,12 @@ package poo.pilhas;
 
 public class ASimpleStackDemo {
 	public static void main(String[] args) {
-		ASimpleStack s;
+			ASimpleStack s;
 		FixedLengthStack fls = new FixedLengthStack(9);
 		FixedLengthStack fls1 = new FixedLengthStack();
-
+		DynamicStack ds = new DynamicStack(9);
+		DynamicStack ds1 = new DynamicStack();
+		
 		fls.push('p');
 		fls.push('r');
 		fls.push('o');
@@ -16,10 +18,23 @@ public class ASimpleStackDemo {
 		fls.push('a');
 		fls.push('r');
 		fls.push('j');
+		
 		fls.pop();
-		fls1.pop();
-
-		System.out.println(fls.pop());
+		/*fls1.pop();
+		ds.pop();
+		ds1.pop();*/
+		
+		
+		
+		System.out.println(ds.size());
+		System.out.println(ds1.size());
+		System.out.println(fls1.size());
+		System.out.println(fls.size());
+		
+		System.out.println(ds.peek());
+		System.out.println(ds1.peek());
+		System.out.println(fls1.peek());
+		System.out.println(fls.peek());
 
 	}
 }
